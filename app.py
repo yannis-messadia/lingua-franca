34081ca99817e00832c586f34acc02c1d6c34b55
yannis-messadia <yannis.messadia@laplateforme.io>
2025-05-18 19:36:50 +0200

    It works, matrix style

--- app.py
+++ app.py
@@ -1,17 +1,32 @@
 from flask import Flask, render_template, request
+from googletrans import Translator
 
 app = Flask(__name__)
+translator = Translator()
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     translation = ''
+    detected_lang = ''
     if request.method == 'POST':
         original_text = request.form['text']
         source_lang = request.form['source_lang']
         target_lang = request.form['target_lang']
-        # La traduction sera ajoutée ici plus tard
-        translation = f"(traduction simulée de: '{original_text}' vers {target_lang})"
-    return render_template('index.html', translation=translation)
+
+        try:
+            if source_lang == 'auto':
+                result = translator.translate(original_text, dest=target_lang)
+            else:
+                result = translator.translate(original_text, src=source_lang, dest=target_lang)
+
+            translation = result.text
+            if source_lang == 'auto':
+                detected_lang = f"(Detected language: {result.src})"
+        except Exception as e:
+            translation = f"Erreur lors de la traduction : {str(e)}"
+
+    return render_template('index.html', translation=translation, detected_lang=detected_lang)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
